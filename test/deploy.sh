96c1f0626c3fed75229bb78a7a7153dec12a7724
denis.m <denis.m@atri.tech>
2023-05-21 18:31:30 +0300

    changed dist

--- test/deploy.sh
+++ test/deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:Denvor228/Remember_me.git master: new-deplyment-branch
+git push -f git@github.com:Denvor228/Remember_me.git main
 
 cd -
